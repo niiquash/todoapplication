e597fa3e705207d486c7b6ca1b78dce3b4cb2b22
niiquash <qua16006@byui.edu>
2023-07-03 23:38:52 -0600

    starting over

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,5 @@
-import { FormEvent, useState } from "react";
-import Header from "./components/Header";
-import TaskList from "./components/TaskList";
-import TodoForm from "./components/TodoForm";
-
 function App() {
-  const [todoItems, setTodoItems] = useState([
-    {
-      id: 1,
-      task: "eat fufu",
-      completed: false,
-    },
-    {
-      id: 2,
-      task: "eat tofu",
-      completed: false,
-    },
-    {
-      id: 3,
-      task: "eat snafu",
-      completed: false,
-    },
-  ]);
-
-  const handleDelete = (id: number) => {
-    setTodoItems(todoItems.filter(item => item.id !== id))
-  }
-
-  const onSubmit = (data: string) => {
-    setTodoItems([...todoItems, { task: data, id: todoItems.length+1, completed: false}])
-  } 
-
-  return (
-    <div>
-      <Header />
-      <TodoForm onSubmit={(data)=>onSubmit(data)} />
-      <TaskList handleDelete={handleDelete} todoItems={todoItems} />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
